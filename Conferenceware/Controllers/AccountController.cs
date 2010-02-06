8f90e02e1f6e199f1de8e05e8917840a837869b5
Jim Wordelman <quad341@gmail.com>
2010-02-06 16:39:30 -0600

    Added default settings file and reformatted example code to show effects

--- Conferenceware/Controllers/AccountController.cs
+++ Conferenceware/Controllers/AccountController.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 using System.Security.Principal;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using System.Web.Security;
@@ -11,194 +8,202 @@ using Conferenceware.Models;
 
 namespace Conferenceware.Controllers
 {
+	[HandleError]
+	public class AccountController : Controller
+	{
+		// This constructor is used by the MVC framework to instantiate the 
+		// controller using the default forms authentication and membership 
+		// providers.
+		public AccountController()
+			: this(null, null)
+		{
+		}
 
-    [HandleError]
-    public class AccountController : Controller
-    {
+		// This constructor is not used by the MVC framework but is instead 
+		// provided for ease of unit testing this type. See the comments in 
+		// AccountModels.cs for more information.
+		public AccountController(IFormsAuthenticationService formsService,
+		                         IMembershipService membershipService)
+		{
+			FormsService = formsService ?? new FormsAuthenticationService();
+			MembershipService = membershipService ?? new AccountMembershipService();
+		}
 
-        // This constructor is used by the MVC framework to instantiate the controller using
-        // the default forms authentication and membership providers.
-        public AccountController()
-            : this(null, null)
-        {
-        }
+		public IFormsAuthenticationService FormsService { get; private set; }
 
-        // This constructor is not used by the MVC framework but is instead provided for ease
-        // of unit testing this type. See the comments in AccountModels.cs for more information.
-        public AccountController(IFormsAuthenticationService formsService, IMembershipService membershipService)
-        {
-            FormsService = formsService ?? new FormsAuthenticationService();
-            MembershipService = membershipService ?? new AccountMembershipService();
-        }
+		public IMembershipService MembershipService { get; private set; }
 
-        public IFormsAuthenticationService FormsService
-        {
-            get;
-            private set;
-        }
+		protected override void Initialize(RequestContext requestContext)
+		{
+			if (requestContext.HttpContext.User.Identity is WindowsIdentity)
+			{
+				throw new InvalidOperationException(
+					"Windows authentication is not supported.");
+			}
+			else
+			{
+				base.Initialize(requestContext);
+			}
+		}
 
-        public IMembershipService MembershipService
-        {
-            get;
-            private set;
-        }
+		protected override void OnActionExecuting(ActionExecutingContext filterContext)
+		{
+			ViewData["PasswordLength"] = MembershipService.MinPasswordLength;
 
-        protected override void Initialize(RequestContext requestContext)
-        {
-            if (requestContext.HttpContext.User.Identity is WindowsIdentity)
-            {
-                throw new InvalidOperationException("Windows authentication is not supported.");
-            }
-            else
-            {
-                base.Initialize(requestContext);
-            }
-        }
+			base.OnActionExecuting(filterContext);
+		}
 
-        protected override void OnActionExecuting(ActionExecutingContext filterContext)
-        {
-            ViewData["PasswordLength"] = MembershipService.MinPasswordLength;
+		[Authorize]
+		public ActionResult ChangePassword()
+		{
+			return View();
+		}
 
-            base.OnActionExecuting(filterContext);
-        }
+		[Authorize]
+		[HttpPost]
+		public ActionResult ChangePassword(ChangePasswordModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				if (MembershipService.ChangePassword(User.Identity.Name,
+				                                     model.OldPassword,
+				                                     model.NewPassword))
+				{
+					return RedirectToAction("ChangePasswordSuccess");
+				}
+				else
+				{
+					ModelState.AddModelError("",
+					                         "The current password is incorrect or the new password is invalid.");
+				}
+			}
 
-        [Authorize]
-        public ActionResult ChangePassword()
-        {
-            return View();
-        }
+			// If we got this far, something failed, redisplay form
+			return View(model);
+		}
 
-        [Authorize]
-        [HttpPost]
-        public ActionResult ChangePassword(ChangePasswordModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                if (MembershipService.ChangePassword(User.Identity.Name, model.OldPassword, model.NewPassword))
-                {
-                    return RedirectToAction("ChangePasswordSuccess");
-                }
-                else
-                {
-                    ModelState.AddModelError("", "The current password is incorrect or the new password is invalid.");
-                }
-            }
+		public ActionResult ChangePasswordSuccess()
+		{
+			return View();
+		}
 
-            // If we got this far, something failed, redisplay form
-            return View(model);
-        }
+		public ActionResult LogOff()
+		{
+			FormsService.SignOut();
 
-        public ActionResult ChangePasswordSuccess()
-        {
-            return View();
-        }
+			return RedirectToAction("Index", "Home");
+		}
 
-        public ActionResult LogOff()
-        {
-            FormsService.SignOut();
+		public ActionResult LogOn()
+		{
+			return View();
+		}
 
-            return RedirectToAction("Index", "Home");
-        }
+		[HttpPost]
+		[SuppressMessage("Microsoft.Design", "CA1054:UriParametersShouldNotBeStrings",
+			Justification = "Needs to take same parameter type as Controller.Redirect()")
+		]
+		public ActionResult LogOn(LogOnModel model, string returnUrl)
+		{
+			if (ModelState.IsValid)
+			{
+				if (MembershipService.ValidateUser(model.UserName, model.Password))
+				{
+					FormsService.SignIn(model.UserName, model.RememberMe);
+					if (!String.IsNullOrEmpty(returnUrl))
+					{
+						return Redirect(returnUrl);
+					}
+					else
+					{
+						return RedirectToAction("Index", "Home");
+					}
+				}
+				else
+				{
+					ModelState.AddModelError("",
+					                         "The user name or password provided is incorrect.");
+				}
+			}
 
-        public ActionResult LogOn()
-        {
-            return View();
-        }
+			// If we got this far, something failed, redisplay form
+			return View(model);
+		}
 
-        [HttpPost]
-        [SuppressMessage("Microsoft.Design", "CA1054:UriParametersShouldNotBeStrings",
-            Justification = "Needs to take same parameter type as Controller.Redirect()")]
-        public ActionResult LogOn(LogOnModel model, string returnUrl)
-        {
-            if (ModelState.IsValid)
-            {
-                if (MembershipService.ValidateUser(model.UserName, model.Password))
-                {
-                    FormsService.SignIn(model.UserName, model.RememberMe);
-                    if (!String.IsNullOrEmpty(returnUrl))
-                    {
-                        return Redirect(returnUrl);
-                    }
-                    else
-                    {
-                        return RedirectToAction("Index", "Home");
-                    }
-                }
-                else
-                {
-                    ModelState.AddModelError("", "The user name or password provided is incorrect.");
-                }
-            }
+		public ActionResult Register()
+		{
+			return View();
+		}
 
-            // If we got this far, something failed, redisplay form
-            return View(model);
-        }
+		[HttpPost]
+		public ActionResult Register(RegisterModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				// Attempt to register the user
+				MembershipCreateStatus createStatus =
+					MembershipService.CreateUser(model.UserName, model.Password, model.Email);
 
-        public ActionResult Register()
-        {
-            return View();
-        }
+				if (createStatus == MembershipCreateStatus.Success)
+				{
+					FormsService.SignIn(model.UserName, false /* createPersistentCookie */);
+					return RedirectToAction("Index", "Home");
+				}
+				else
+				{
+					ModelState.AddModelError("", ErrorCodeToString(createStatus));
+				}
+			}
 
-        [HttpPost]
-        public ActionResult Register(RegisterModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                // Attempt to register the user
-                MembershipCreateStatus createStatus = MembershipService.CreateUser(model.UserName, model.Password, model.Email);
+			// If we got this far, something failed, redisplay form
+			return View(model);
+		}
 
-                if (createStatus == MembershipCreateStatus.Success)
-                {
-                    FormsService.SignIn(model.UserName, false /* createPersistentCookie */);
-                    return RedirectToAction("Index", "Home");
-                }
-                else
-                {
-                    ModelState.AddModelError("", ErrorCodeToString(createStatus));
-                }
-            }
+		private static string ErrorCodeToString(MembershipCreateStatus createStatus)
+		{
+			// See http://go.microsoft.com/fwlink/?LinkID=177550 for
+			// a full list of status codes.
+			switch (createStatus)
+			{
+				case MembershipCreateStatus.DuplicateUserName:
+					return "Username already exists. Please enter a different user name.";
 
-            // If we got this far, something failed, redisplay form
-            return View(model);
-        }
+				case MembershipCreateStatus.DuplicateEmail:
+					return
+						"A username for that e-mail address already exists. Please enter a different e-mail address.";
 
-        private static string ErrorCodeToString(MembershipCreateStatus createStatus)
-        {
-            // See http://go.microsoft.com/fwlink/?LinkID=177550 for
-            // a full list of status codes.
-            switch (createStatus)
-            {
-                case MembershipCreateStatus.DuplicateUserName:
-                    return "Username already exists. Please enter a different user name.";
+				case MembershipCreateStatus.InvalidPassword:
+					return
+						"The password provided is invalid. Please enter a valid password value.";
 
-                case MembershipCreateStatus.DuplicateEmail:
-                    return "A username for that e-mail address already exists. Please enter a different e-mail address.";
+				case MembershipCreateStatus.InvalidEmail:
+					return
+						"The e-mail address provided is invalid. Please check the value and try again.";
 
-                case MembershipCreateStatus.InvalidPassword:
-                    return "The password provided is invalid. Please enter a valid password value.";
+				case MembershipCreateStatus.InvalidAnswer:
+					return
+						"The password retrieval answer provided is invalid. Please check the value and try again.";
 
-                case MembershipCreateStatus.InvalidEmail:
-                    return "The e-mail address provided is invalid. Please check the value and try again.";
+				case MembershipCreateStatus.InvalidQuestion:
+					return
+						"The password retrieval question provided is invalid. Please check the value and try again.";
 
-                case MembershipCreateStatus.InvalidAnswer:
-                    return "The password retrieval answer provided is invalid. Please check the value and try again.";
+				case MembershipCreateStatus.InvalidUserName:
+					return
+						"The user name provided is invalid. Please check the value and try again.";
 
-                case MembershipCreateStatus.InvalidQuestion:
-                    return "The password retrieval question provided is invalid. Please check the value and try again.";
+				case MembershipCreateStatus.ProviderError:
+					return
+						"The authentication provider returned an error. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
 
-                case MembershipCreateStatus.InvalidUserName:
-                    return "The user name provided is invalid. Please check the value and try again.";
+				case MembershipCreateStatus.UserRejected:
+					return
+						"The user creation request has been canceled. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
 
-                case MembershipCreateStatus.ProviderError:
-                    return "The authentication provider returned an error. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
-
-                case MembershipCreateStatus.UserRejected:
-                    return "The user creation request has been canceled. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
-
-                default:
-                    return "An unknown error occurred. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
-            }
-        }
-
-    }
+				default:
+					return
+						"An unknown error occurred. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
+			}
+		}
+	}
 }
