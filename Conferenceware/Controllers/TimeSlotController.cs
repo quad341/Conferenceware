7d4cd5c62f3998911dd941b25c1fbaa8bfcd84c0
Daniel Freedman <dfreedm2@uiuc.edu>
2010-02-13 15:17:34 -0600

    Yeah, there we go

---
+++ Conferenceware/Controllers/TimeSlotController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Conferenceware.Controllers
+{
+    public class TimeSlotController : Controller
+    {
+        //
+        // GET: /TimeSlots/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
