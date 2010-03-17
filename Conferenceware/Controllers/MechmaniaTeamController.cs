9bed8e8956352a294a84735b6f6edf6eb6314711
Jim Wordelman <quad341@gmail.com>
2010-03-16 19:36:24 -0500

    working toward admin mmteam controller

--- Conferenceware/Controllers/MechmaniaTeamController.cs
+++ Conferenceware/Controllers/MechmaniaTeamController.cs
@@ -3,100 +3,100 @@ using Conferenceware.Models;
 
 namespace Conferenceware.Controllers
 {
-    public class MechmaniaTeamController : Controller
-    {
-        //
-        // GET: /MechmaniaTeam/
+	public class MechManiaTeamController : Controller
+	{
+		//
+		// GET: /MechmaniaTeam/
 
-        public ActionResult Index()
-        {
-            return View();
-        }
+		public ActionResult Index()
+		{
+			return View();
+		}
 
-        //
-        // GET: /MechmaniaTeam/Details/5
+		//
+		// GET: /MechmaniaTeam/Details/5
 
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
+		public ActionResult Details(int id)
+		{
+			return View();
+		}
 
-        //
-        // GET: /MechmaniaTeam/Create
+		//
+		// GET: /MechmaniaTeam/Create
 
-        public ActionResult Create()
-        {
-            return View();
-        } 
+		public ActionResult Create()
+		{
+			return View();
+		}
 
-        //
-        // POST: /MechmaniaTeam/Create
+		//
+		// POST: /MechmaniaTeam/Create
 
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
+		[HttpPost]
+		public ActionResult Create(FormCollection collection)
+		{
+			try
+			{
+				// TODO: Add insert logic here
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-        
-        //
-        // GET: /MechmaniaTeam/Edit/5
- 
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
+				return RedirectToAction("Index");
+			}
+			catch
+			{
+				return View();
+			}
+		}
 
-        //
-        // POST: /MechmaniaTeam/Edit/5
+		//
+		// GET: /MechmaniaTeam/Edit/5
 
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
- 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+		public ActionResult Edit(int id)
+		{
+			return View();
+		}
 
-        //
-        // GET: /MechmaniaTeam/Delete/5
- 
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
+		//
+		// POST: /MechmaniaTeam/Edit/5
 
-        //
-        // POST: /MechmaniaTeam/Delete/5
+		[HttpPost]
+		public ActionResult Edit(int id, FormCollection collection)
+		{
+			try
+			{
+				// TODO: Add update logic here
 
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
- 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-    }
+				return RedirectToAction("Index");
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		//
+		// GET: /MechmaniaTeam/Delete/5
+
+		public ActionResult Delete(int id)
+		{
+			return View();
+		}
+
+		//
+		// POST: /MechmaniaTeam/Delete/5
+
+		[HttpPost]
+		public ActionResult Delete(int id, FormCollection collection)
+		{
+			try
+			{
+				// TODO: Add delete logic here
+
+				return RedirectToAction("Index");
+			}
+			catch
+			{
+				return View();
+			}
+		}
+	}
 }
---
+++ Conferenceware/Models/MechManiaTeamAdminEditData.cs
@@ -0,0 +1,19 @@
+using System.Web.Mvc;
+
+namespace Conferenceware.Models
+{
+	public class MechManiaTeamAdminEditData
+	{
+		public MechManiaTeam MechManiaTeam
+		{
+			get;
+			set;
+		}
+
+		public SelectList Attendees
+		{
+			get;
+			set;
+		}
+	}
+}
