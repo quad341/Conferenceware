ca9ad662d4fba31b14660ae8367f384a581f0322
quad341 <quad341@gmail.com>
2010-09-01 23:26:18 -0500

    added titles for food and tshirts for volunteers (in metadata)

--- Conferenceware/Models/VolunteerMetadata.cs
+++ Conferenceware/Models/VolunteerMetadata.cs
@@ -4,6 +4,12 @@ namespace Conferenceware.Models
 {
 	public class VolunteerMetadata
 	{
+		[DisplayName("T-Shirt Size")]
+		public int tshirt_id { get; set; }
+
+		[DisplayName("Food Choice")]
+		public int food_choice_id { get; set; }
+		
 		[DisplayName("Is video trained?")]
 		public bool is_video_trained { get; set; }
 	}
